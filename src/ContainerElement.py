ea77fa6f2b66db83f880e2ca607299f2f669e930
Lucas Campos <achcarlucas@hotmail.com>
2022-05-11 00:12:02 -0300

    Criado o ContainerElement que será responsável por conter os elementos químicos na tela

---
+++ src/ContainerElement.py
@@ -0,0 +1,9 @@
+from src import ElementPackage
+from src import GameInterface
+
+class Container(GameInterface.GameInterface):
+
+    def __init__(self, elementPackage : ElementPackage.ElementPackage):
+        self._elementPackage = elementPackage;
+
+
