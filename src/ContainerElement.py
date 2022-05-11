c42f86047b3d3fe70b82f52c6ee2481fbb4bfa4f
Lucas Campos <achcarlucas@hotmail.com>
2022-05-11 01:08:14 -0300

    deltaTime na função update

--- src/ContainerElement.py
+++ src/ContainerElement.py
@@ -6,7 +6,7 @@ class Container(GameInterface.GameInterface):
     def __init__(self, elementPackage : ElementPackage.ElementPackage):
         self._elementPackage = elementPackage;
 
-    def update(self):
+    def update(self, deltaTime):
         pass
 
     def render(self, screen : GameInterface.Surface):
