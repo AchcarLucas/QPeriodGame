4e24c270f22ac675c604b5579512f8ea36daa80a
Achcar Lucas <achcarlucas@gmail.com>
2022-05-17 10:02:52 -0300

    ContainerTable adicionado

---
+++ src/ContainerTable.py
@@ -0,0 +1,9 @@
+import pygame
+
+from src import Container
+from src import ElementPackage
+from src import GameInterface
+
+class ContainerTable(GameInterface.GameInterface):
+    def __init__(self):
+        pass
